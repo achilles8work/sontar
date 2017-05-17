3a7d4cc1ab343b23af108676e6357de4de2d18cf
Abhishek Kumar Singh <abhi8jss@gmail.com>
2017-05-17 15:49:51 +0530

    Update settings.py

--- sontar/settings.py
+++ sontar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h4g20b-gj@bjw_fh08*za75$0v4(9q-$ou@$y3i*08^870ru&c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.154']
 
 
 # Application definition
